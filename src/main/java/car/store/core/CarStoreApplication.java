fbebb04c45e39c0a6d7fe7ba310b3ba8cb33a09c
Kirill Privalov <kirbimerish@gmail.com>
2018-07-13 23:58:47 +0300

    Создание проекта

---
+++ src/main/java/car/store/core/CarStoreApplication.java
@@ -0,0 +1,12 @@
+package car.store.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarStoreApplication.class, args);
+    }
+}
