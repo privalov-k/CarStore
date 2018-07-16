8429b1b9b74b2119e35010a8a7e58451feec3396
Kirill Privalov <kirbimerish@gmail.com>
2018-07-16 23:47:35 +0300

    Вывод Hello World на localhost:8080/hello

--- src/main/java/car/store/core/CarStoreApplication.java
+++ src/main/java/car/store/core/CarStoreApplication.java
@@ -2,8 +2,11 @@ package car.store.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "car.store")
 public class CarStoreApplication {
 
     public static void main(String[] args) {
